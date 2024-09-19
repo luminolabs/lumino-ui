e28e6f4cf6b347fcc35213a7e8ac029f43bcde1d
Shyam Patel <shyampkira@gmail.com>
2024-09-19 13:22:33 -0700

    Add logo and redirect to external link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,7 @@
-'use client'
-
-import { Box, Flex, Spacer, Link, Image } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Flex, Spacer, Link, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { FiClock } from 'react-icons/fi';
+import Image from 'next/image';
 
 const Header = () => {
   return (
@@ -10,21 +9,27 @@ const Header = () => {
       <Flex maxW="container.xl" mx="auto" alignItems="center">
         <NextLink href="/" passHref>
           <Flex as="a" alignItems="center">
-            <Box as={FiClock} color="purple.500" fontSize="24px" mr={2} />
-            <Box as="span" fontSize="xl" fontWeight="bold" color="purple.700">
-              lumino
+            <Box position="absolute" width="128px" height="32px">
+              <Image
+                src="/logo.svg"
+                alt="Lumino Logo"
+                layout="fill"
+                objectFit="contain"
+              />
             </Box>
+            {/* <Text fontSize="xl" fontWeight="bold" color="purple.700">
+              lumino
+            </Text> */}
           </Flex>
         </NextLink>
         <Spacer />
         <Flex gap={6}>
-          <Link as={NextLink} href="/dashboard" color="gray.600">
+          {/* <Link as={NextLink} href="/dashboard" color="gray.600">
             Dashboard
-          </Link>
-          <Link as={NextLink} href="/docs" color="gray.600">
+          </Link> */}
+          <Link as={NextLink} href="https://docs.luminolabs.ai" target="_blank" color="gray.600">
             Docs
           </Link>
-          <Box as={FiClock} color="gray.600" fontSize="20px" />
         </Flex>
       </Flex>
     </Box>
