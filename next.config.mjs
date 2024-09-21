120d529886702d381addf6c00efacd9e16c8247f
Shyam Patel <shyampkira@gmail.com>
2024-09-20 17:06:51 -0700

    nit: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   API_BASE_URL: process.env.LUI_API_BASE_URL,
-  // },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/proxy/:path*",
-  //       destination: `${process.env.LUI_API_BASE_URL || "http://localhost:5100/v1/"}:path*`,
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
