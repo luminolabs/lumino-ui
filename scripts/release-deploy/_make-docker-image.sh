9dcb043f88da3173fe0b1003d3aff483e5a33176
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-09 13:28:21 -0700

    LUM-841: Automated deployments

--- scripts/release-deploy/_make-docker-image.sh
+++ scripts/release-deploy/_make-docker-image.sh
@@ -10,11 +10,11 @@ source /$SERVICE_NAME/scripts/utils.sh  # $SERVICE_NAME is set in make-deploymen
 cd /$SERVICE_NAME
 
 # Build, tag, and push the Docker image
-echo "Building the Docker image..."
+echo "Building the Docker image; version $(cat VERSION)..."
 docker build -t $SERVICE_NAME:local .
 docker tag $SERVICE_NAME:local $ARTIFACT_REPO_URL/$SERVICE_NAME:$(cat VERSION)
 docker tag $SERVICE_NAME:local $ARTIFACT_REPO_URL/$SERVICE_NAME:latest
 echo "Pushing the Docker image..."
 docker push $ARTIFACT_REPO_URL/$SERVICE_NAME:$(cat VERSION)
 docker push $ARTIFACT_REPO_URL/$SERVICE_NAME:latest
-echo "Done."
+echo "Done; version $(cat VERSION)"
