f5733dfb3ff3ff21dffd27b15bd97ada07a371c3
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-09 13:24:17 -0700

    LUM-841: Automated deployments

--- scripts/release-deploy/make-deployment.sh
+++ scripts/release-deploy/make-deployment.sh
@@ -11,5 +11,6 @@ source ./scripts/utils.sh  # Sets $SERVICE_NAME
 stty -echo  # Hide the user input, so the password is not displayed
 gcloud compute ssh $BUILD_VM --project=$PROJECT_ID --zone=$WORK_ZONE \
       --command="SERVICE_NAME=$SERVICE_NAME /$SERVICE_NAME/scripts/release-deploy/_git-pull.sh"
+stty echo  # Restore the user input
 gcloud compute ssh $BUILD_VM --project=$PROJECT_ID --zone=$WORK_ZONE \
       --command="SERVICE_NAME=$SERVICE_NAME /$SERVICE_NAME/scripts/release-deploy/_make-docker-image.sh"
